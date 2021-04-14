540a08a016e7082baf186403ea0d1855c5d89739
prppgn <prppgn@gmail.com>
2021-04-14 23:32:39 +0200

    Update assigment1.py

--- assignment_basic_1/assigment1.py
+++ assignment_basic_1/assigment1.py
@@ -1,5 +1,15 @@
 import argparse
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+
+def process(file_path):
+  logging.warning("Reading input file %s...", file_path)  #c'è la virgola in modo che se il messaggio non viene stampato lui non lo formatta e risparmia tempo
 
 if __name__ == '__main__':        #cose eseguite solo quando il file viene eseguito direttamente
   parser = argparse.ArgumentParser()
-  parser
+  parser.add_argument('infile', type=str, help='Path to the input file')   #stringa con il path del file di input
+  args = parser.parse_args()
+
+
+  process(args.infile)
