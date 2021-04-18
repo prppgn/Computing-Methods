7468c1b4a4cabda156fd45061ef91216bf7157eb
prppgn <prppgn@gmail.com>
2021-04-18 13:54:11 +0200

    Update assigment1.py
    Fine lezione 6

--- assignment_basic_1/assigment1.py
+++ assignment_basic_1/assigment1.py
@@ -4,6 +4,7 @@
 import argparse
 import logging
 import time
+import string
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,12 +22,29 @@ def process(file_path): # c'è la virgola in modo che se non stampa il messaggio
     # il professore trova 1214387 caratteri
     # io ne trovo 1214416, qualcosa non va
 
-    char_dict = {chr(x): 0 for x in range(ord('a'), ord('z') + 1)}
+    #char_dict = {chr(x): 0 for x in range(ord('a'), ord('z') + 1)}
     # ord() da il numero d'ordine di un carattere (vedi tabella ASCII)
     # chr(x) da il carattere di numero d'ordine x
 
+    char_dict = {ch: 0 for ch in string.ascii_lowercase}
+    # piu pythonico del precedente
+    for ch in text:
+        # i 2 modi seguenti sono ugualmente pythonici
+        ch=ch.lower()
+        if ch in char_dict:
+            char_dict[ch] += 1
+
+        # try: # se il valore e' nelle chiavi gia' inserite, incrementa il valore
+        #     char_dict[ch.lower()] += 1
+        # except KeyError: # altrimenti non fa fiente
+        #     pass
+
     elapsed_time = time.time() - start_time
     logging.info("Done in %.3f seconds", elapsed_time)
+    num_letters = sum(char_dict.values())
+    print(num_letters)
+    for ch, num in char_dict.items():
+        print(f"{ch} ->{num / num_letters:.3%}")  #il formato % moltiplica per 100 e da la percentuale
     print(char_dict)
 
 
